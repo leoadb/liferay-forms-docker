f058d1b4f4d21e710313f80fa4e8d6385dba0dd8
Manoel Cyreno <manoel.cyreno@liferay.com>
2018-12-06 17:20:16 -0300

    Improve scripts and add a new script

--- run.sh
+++ run.sh
@@ -2,18 +2,13 @@
 
 read -p 'What is the ticket number: ' ticket
 
-echo 'Where do you want to pull the Docker image?\n1 - Local (default)\n2 - Docker Hub (external)'
-
+echo 'Where do you want to pull the Docker image?'
+echo '1 - Local (default)'
+echo '2 - Docker Hub (external)'
 read repository
 
 if [ -z "$repository" ]; then
   repository=1
-fi
-
-read -p 'What is the port number you want to run the server (8080): ' port
-
-if [ -z "$port" ]; then
-  port=8080
 fi
 
 container=$(docker ps -a | grep "$ticket" | wc -l)
@@ -25,8 +20,31 @@ fi
 
 if [ $repository -eq 1 ]; then
   docker pull 192.168.109.41:5000/liferay/com-liferay-forms:$ticket
-  docker run -p $port:8080 --name $ticket 192.168.109.41:5000/liferay/com-liferay-forms:$ticket
+  docker run -d -p 0:8080 --name $ticket 192.168.109.41:5000/liferay/com-liferay-forms:$ticket
 elif [ $repository -eq 2 ]; then
   docker pull liferay/com-liferay-forms-qa:$ticket
-  docker run -p $port:8080 --name $ticket liferay/com-liferay-forms-qa:$ticket
+  docker run -d -p 0:8080 --name $ticket liferay/com-liferay-forms-qa:$ticket
+fi
+
+function findPort(){
+  docker port $ticket >> tempText.txt
+  sed 's/.\{19\}//' tempText.txt >> tempText2.txt
+}
+
+function removeTempText(){
+  rm -rf tempText.txt tempText2.txt
+}
+
+container2=$(docker ps -a | grep "$ticket" | wc -l)
+
+if [ $container2 -eq 1 ]; then
+  findPort
+  echo ""
+  echo "You can access your docker by the url: "
+  echo "http://localhost"$(cat tempText2.txt)
+  echo ""
+  removeTempText
+
+  echo 'Your docker will be listed below:'
+  docker ps -a | grep $ticket
 fi
